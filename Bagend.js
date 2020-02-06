1ad484b4f897c86622e37d5f3a88d82470bddd8d
newmat123 <37107989+newmat123@users.noreply.github.com>
2020-02-06 13:28:07 +0100

    i

--- Bagend.js
+++ Bagend.js
@@ -1,1 +1,11 @@
 //key  4cb44547e51e4695b6a73b9e1a60c3ffbe5c4989
+//exercise
+//exerciseinfo
+//exercisecategory
+//exercisecomment
+//exerciseimage
+  //"exerciseinfo": "http://wger.de/api/v2/exerciseinfo/",
+  //"exercise": "http://wger.de/api/v2/exercise/",
+  //"exercisecategory": "http://wger.de/api/v2/exercisecategory/",
+  //"exerciseimage": "http://wger.de/api/v2/exerciseimage/",
+  //"exercisecomment": "http://wger.de/api/v2/exercisecomment/",
