05a9924a90b745a730a2ada01621c2b6ffc74a8a
newmat123 <37107989+newmat123@users.noreply.github.com>
2020-02-11 15:03:58 +0100

    idk

--- script.js
+++ script.js
@@ -9,6 +9,10 @@
   //"exercisecategory": "http://wger.de/api/v2/exercisecategory/",
   //"exerciseimage": "http://wger.de/api/v2/exerciseimage/",
   //"exercisecomment": "http://wger.de/api/v2/exercisecomment/",
+
+//root
+//http://wger.de/api/v2/
+
 const app = document.getElementById('root');
 
 const container = document.createElement('div');
@@ -16,23 +20,27 @@ container.setAttribute('class', 'container');
 
 app.appendChild(container);
 
+
+
 var request = new XMLHttpRequest();
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.open('GET', 'http://wger.de/api/v2/exerciseinfo/?format=api', true);
+
 request.onload = function () {
 
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
+
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
+    data.forEach(results => {
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
 
       const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
+      h1.textContent = results.name;
 
       const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
+      results.description = results.description.substring(0, 300);
+      p.textContent = `${results.description}...`;
 
       container.appendChild(card);
       card.appendChild(h1);
@@ -45,4 +53,46 @@ request.onload = function () {
   }
 }
 
+request.setRequestHeader('Authorization', '4cb44547e51e4695b6a73b9e1a60c3ffbe5c4989');
 request.send();
+
+
+
+
+
+//const app = document.getElementById('root');
+
+//const container = document.createElement('div');
+//container.setAttribute('class', 'container');
+
+//app.appendChild(container);
+
+//var request = new XMLHttpRequest();
+//request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+//request.onload = function () {
+
+  // Begin accessing JSON data here
+//  var data = JSON.parse(this.response);
+//  if (request.status >= 200 && request.status < 400) {
+//    data.forEach(movie => {
+//      const card = document.createElement('div');
+//      card.setAttribute('class', 'card');
+
+//      const h1 = document.createElement('h1');
+//      h1.textContent = movie.title;
+
+//      const p = document.createElement('p');
+//      movie.description = movie.description.substring(0, 300);
+//      p.textContent = `${movie.description}...`;
+
+//      container.appendChild(card);
+//      card.appendChild(h1);
+//      card.appendChild(p);
+//    });
+//  } else {
+//    const errorMessage = document.createElement('marquee');
+//    errorMessage.textContent = `Gah, it's not working!`;
+//  }
+//}
+
+//request.send();
