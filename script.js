1e97a4c53ce230b746a8076ff6ce95084b895486
newmat123 <37107989+newmat123@users.noreply.github.com>
2020-02-16 17:55:25 +0100

    i

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 //http://wger.de/en/software/api
 
-var muscleGroup;
+//muskel gruppen brugeren kigger på
+var MuscleGroup;
+
+//rooten af apien
+const Url = 'https://wger.de/api/v2/';
+
+//holder kategorierne
+const CatHolder = document.getElementById('categories');
+
+//finner og holder containerens plads
+const Main = document.getElementById('root');
+const Container = document.createElement('div');
+Container.setAttribute('class', 'container');
+Main.appendChild(Container);
 
 
-const catHolder = document.getElementById('categories');
-const main = document.getElementById('root');
-
-const container = document.createElement('div');
-container.setAttribute('class', 'container');
-
-main.appendChild(container);
 
 
-const apiUrl_exsersice = 'http://wger.de/api/v2/exercise/';
-const apiUrl_img = 'http://wger.de/api/v2/exerciseimage/';
-const exsersiceCategoriUrl = 'http://wger.de/api/v2/exercisecategory/';
+
 
 
 //card example
@@ -31,7 +35,7 @@ const imge = document.createElement('img');
 imge.setAttribute('class', 'cardImg');
 imge.setAttribute('src', 'imges/Hammer-Curls.jpg');
 
-container.appendChild(card);
+Container.appendChild(card);
 card.appendChild(h1);
 card.appendChild(p);
 card.appendChild(imge);
@@ -39,31 +43,37 @@ card.appendChild(imge);
 
 
 
-async function categorys() {
-  const response = await fetch(exsersiceCategoriUrl);
-  const data = await response.json();
-  const {results} = data;
 
-  results.forEach(cats => {
+
+
+//henter de forskellige kategorier
+async function categorys() {
+
+  const response = await fetch(Url + 'exercisecategory/');
+  const data = await response.json();
+
+  data.results.forEach(cats => {
     const element = document.createElement('div');
     element.setAttribute('class', 'muscelgroups');
     element.setAttribute('onclick', 'defineMuscle('+cats.id+')');
     element.innerHTML = cats.name;
 
-    catHolder.appendChild(element);
+    CatHolder.appendChild(element);
 
   });
 }
 
 
-
+//lopper iggennem den givne api og danner kortne
 async function processApi(url) {
+  //henter data
   const response = await fetch(url);
   const data = await response.json();
-  const {results} = data;
 
-  results.forEach(exsersice => {
-    if(exsersice.name_original != "" && exsersice.description != "" && exsersice.category == muscleGroup && exsersice.language == 2){
+  //looper gennem alle resultaterne
+  data.results.forEach(exsersice => {
+    //tjækker om der er fyld på, om det er den rigtige kattegori og om det er på engelsk
+    if(exsersice.name_original != "" && exsersice.description != "" && exsersice.category == MuscleGroup && exsersice.language == 2){
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
 
@@ -73,15 +83,16 @@ async function processApi(url) {
       const p = document.createElement('p');
       p.innerHTML = `${exsersice.description}`;
 
-      container.appendChild(card);
+      Container.appendChild(card);
       card.appendChild(h1);
       card.appendChild(p);
 
+      //finder det tilhørende billede
       async function getImg(url){
         const response = await fetch(url);
         const data = await response.json();
-        const {results} = data;
-        results.forEach(image => {
+
+        data.results.forEach(image => {
           if(image.exercise == exsersice.id){
             const imge = document.createElement('img');
             imge.setAttribute('class', 'cardImg');
@@ -90,26 +101,29 @@ async function processApi(url) {
             card.appendChild(imge);
           }
         });
+        //søger for at vi tjækker alle
         if(data.next != null){
           getImg(data.next);
         }
       }
-      getImg(apiUrl_img);
+      getImg(Url+'exerciseimage/');
     }
   });
+  //søger for at vi får alle øvelserne med
   if(data.next != null){
     processApi(data.next);
   }
 }
 
 
+//kaldes når brugeren klikker på en kategori
 function defineMuscle(i) {
-  if(i != muscleGroup){
-    container.innerHTML = "";
-    muscleGroup = i;
-    processApi(apiUrl_exsersice);
+  if(i != MuscleGroup){
+    Container.innerHTML = "";
+    MuscleGroup = i;
+    processApi(Url+'exercise/');
   }
 }
 
-
+//søger for at kategorierne bliver fremvist
 categorys();
